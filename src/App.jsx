2ee6b16da64e913bc0655300c07a9aa93769667a
sajjad khan <sajjadkhan099099@gmail.com>
2025-07-09 12:36:50 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './globals.css'
+import Counter from './Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
