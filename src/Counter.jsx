c473d70b7b73b9ae576a1ac708f9463338ea5781
sajjad khan <sajjadkhan099099@gmail.com>
2025-07-10 03:33:51 -0700

    first commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,28 +1,24 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
-const [count,setcount]= useState (0);
-const Decreasement=()=>{
-    setCount(count - 1);
-}
-  }
+const [count,setCount]= useState(0);
+  
   return (
     <>
       <div className="container mx-auto text-center">
         <h1 className='text-9xl'>{count}</h1>
-        <button onClick={Decreasement} className='text-white cursor-pointer mx-2 rounded-md active:scale-95 px-5 py-2 bg-red-500 hover:bg-red-600 '>
+        <button onClick={() =>setCount(count - 1)} className='text-white cursor-pointer mx-2 rounded-md active:scale-95 px-5 py-2 bg-red-500 hover:bg-red-600 '>
             Decrease
             </button>
-        <button className='text-white cursor-pointer mx-2 rounded-md active:scale-95 px-5 py-2 bg-yellow-500 hover:bg-yellow-600 '>
+        <button onClick={() =>setCount(0)} className='text-white cursor-pointer mx-2 rounded-md active:scale-95 px-5 py-2 bg-yellow-500 hover:bg-yellow-600 '>
             Reset
             </button>
-        <button className='text-white cursor-pointer mx-2 rounded-md active:scale-95 px-5 py-2 bg-green-500 hover:bg-green-600 '>
+        <button onClick={() =>setCount(count + 1)} className='text-white cursor-pointer mx-2 rounded-md active:scale-95 px-5 py-2 bg-green-500 hover:bg-green-600 '>
             increase
             </button>
 
       </div>
     </>
-  )
-}
-
+  );
+};
 export default Counter
